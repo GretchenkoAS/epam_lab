ade5953591dc495127217155c57f3a0a1b264ceb
Andrey Gretchenko <gret.andrey@gmail.com>
2021-08-26 20:22:21 +0300

    module2

---
+++ restapp/src/main/java/com/epam/esm/controller/GiftCertificateController.java
@@ -0,0 +1,117 @@
+package com.epam.esm.controller;
+
+import com.epam.esm.dto.GiftCertificateDto;
+import com.epam.esm.dto.QueryDto;
+import com.epam.esm.exeption.AppException;
+import com.epam.esm.service.GiftCertificateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Provide a centralized request handling mechanism to
+ * handle all types of requests for gift certificate.
+ *
+ * @author Andrey Gretchenko
+ */
+@RestController
+@RequestMapping("/giftCertificates")
+public class GiftCertificateController {
+    private final GiftCertificateService giftCertificateService;
+
+    @Autowired
+    public GiftCertificateController(GiftCertificateService giftCertificateService) {
+        this.giftCertificateService = giftCertificateService;
+    }
+
+    /**
+     * Returns all GiftCertificateDto objects of tags from repository.
+     *
+     * @return list of GiftCertificateDto objects of retrieved gift certificates
+     */
+    @GetMapping
+    public List<GiftCertificateDto> findAll() {
+        return giftCertificateService.findAll();
+    }
+
+    /**
+     * Returns GiftCertificateDto object for gift certificate with provided id from repository.
+     *
+     * @param id id of gift certificate to find
+     * @return GiftCertificateDto object of gift certificate with provided id in repository
+     * @throws AppException if gift certificate with provided id is not present in repository
+     */
+    @GetMapping("/{id}")
+    public GiftCertificateDto find(@PathVariable Long id) {
+        return giftCertificateService.find(id);
+    }
+
+    /**
+     * Adds gift certificate to repository according to provided dto object.
+     *
+     * @param newGiftCertificate GiftCertificateDto object on basis of which is created new gift certificate
+     *                           in repository
+     * @return GiftCertificateDto gift certificate dto of created in repository gift certificate
+     * @throws AppException if fields in provided GiftCertificateDTO object is not valid
+     */
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public GiftCertificateDto add(@RequestBody GiftCertificateDto newGiftCertificate) {
+        return giftCertificateService.add(newGiftCertificate);
+    }
+
+    /**
+     * Removes gift certificate with provided id from repository.
+     *
+     * @param id id of gift certificate to delete from repository
+     */
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        giftCertificateService.delete(id);
+    }
+
+    /**
+     * Updates gift certificate according to request body.
+     *
+     * @param updatedCertificateDto GiftCertificateDto object according to which is necessary to update gift certificate
+     *                              in repository
+     * @param id                    id of updated gift certificate
+     * @return GiftCertificateDto gift certificate dto of updated gift certificate in repository
+     * @throws AppException if fields in provided GiftCertificateDto is not valid or gift certificate with provided
+     *                         id is not present in repository
+     */
+    @PutMapping("/{id}")
+    public GiftCertificateDto update(@RequestBody GiftCertificateDto updatedCertificateDto, @PathVariable Long id) {
+        return giftCertificateService.update(updatedCertificateDto, id);
+    }
+
+    /**
+     * Retrieves gift certificates from repository according to provided request parameters.
+     *
+     * @param queryDto  QueryDto request parameter for search by query
+     * @throws AppException if QueryDto fields is not valid
+     * @return List<GiftCertificate> list of gift certificates from repository according to provided query
+     */
+    @GetMapping("/query")
+    public List<GiftCertificateDto> findByQuery(@RequestBody QueryDto queryDto) {
+        return giftCertificateService.findByQuery(queryDto);
+    }
+
+    /**
+     * Updates gift certificate fields according to provided in request body fields.
+     *
+     * @param updatedCertificateDto GiftCertificateDto object according to which is necessary to update gift certificate
+     *                              in repository
+     * @param id                    id of updated gift certificate
+     * @return GiftCertificateDto gift certificate dto of updated gift certificate in repository
+     * @throws AppException if fields in provided GiftCertificateDto is not valid or gift certificate with provided
+     *                         id is not present in repository
+     */
+    @PatchMapping("/{id}")
+    public GiftCertificateDto patch(@RequestBody GiftCertificateDto updatedCertificateDto, @PathVariable Long id){
+        return giftCertificateService.patch(updatedCertificateDto, id);
+    }
+}
---
+++ service/src/test/java/com/epam/esm/service/impl/GiftCertificateServiceImplTest.java
@@ -0,0 +1,240 @@
+package com.epam.esm.service.impl;
+
+
+import com.epam.esm.dao.GiftCertificateDao;
+import com.epam.esm.dto.GiftCertificateDto;
+import com.epam.esm.dto.QueryDto;
+import com.epam.esm.entity.GiftCertificate;
+import com.epam.esm.entity.Query;
+import com.epam.esm.exeption.AppException;
+import com.epam.esm.mapper.impl.GiftCertificateMapper;
+import com.epam.esm.mapper.impl.QueryMapper;
+import com.epam.esm.service.TagService;
+import com.epam.esm.validator.GiftCertificateValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class GiftCertificateServiceImplTest {
+
+    @InjectMocks
+    private GiftCertificateServiceImpl giftCertificateService;
+    @Mock
+    private GiftCertificateDao giftCertificateDao;
+    @Mock
+    private TagService tagService;
+    @Mock
+    private GiftCertificateMapper mapper;
+    @Mock
+    private GiftCertificateValidator validator;
+    @Mock
+    private QueryMapper queryMapper;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void addGiftCertificatePositiveTest() {
+        GiftCertificateDto expectedDto = new GiftCertificateDto();
+        expectedDto.setName("GiftCertificate");
+        GiftCertificate expected = new GiftCertificate();
+        expected.setName("GiftCertificate");
+
+        when(validator.isValidName(expectedDto.getName())).thenReturn(true);
+        when(validator.isValidDescription(expectedDto.getDescription())).thenReturn(true);
+        when(validator.isValidPrice(expectedDto.getPrice())).thenReturn(true);
+        when(validator.isValidDuration(expectedDto.getDuration())).thenReturn(true);
+        when(mapper.mapToEntity(expectedDto)).thenReturn(expected);
+        when(giftCertificateDao.findByName(expected.getName())).thenReturn(Optional.empty());
+        when(giftCertificateDao.add(expected)).thenReturn(expected);
+
+        when(mapper.mapToDto(expected)).thenReturn(expectedDto);
+
+        GiftCertificateDto actual = giftCertificateService.add(expectedDto);
+        assertEquals(expectedDto, actual);
+        verify(giftCertificateDao, times(1)).add(any(GiftCertificate.class));
+    }
+
+    @Test
+    void addGiftCertificateNegativeTest() {
+        GiftCertificateDto expectedDto = new GiftCertificateDto();
+        expectedDto.setName("GiftCertificate");
+        GiftCertificate expected = new GiftCertificate();
+        expected.setName("GiftCertificate");
+
+        when(validator.isValidName(expectedDto.getName())).thenReturn(false);
+        assertThrows(AppException.class,() -> {
+            giftCertificateService.add(expectedDto);
+        } );
+        verify(giftCertificateDao,  never()).add(any(GiftCertificate.class));
+    }
+
+    @Test
+    public void findPositiveTest() {
+        Long id = 1L;
+        GiftCertificateDto expectedDto = new GiftCertificateDto();
+        expectedDto.setName("GiftCertificate");
+        expectedDto.setId(id);
+        GiftCertificate expected = new GiftCertificate();
+        expected.setName("GiftCertificate");
+        expected.setId(id);
+        when(giftCertificateDao.findOne(id)).thenReturn(Optional.of(expected));
+        when(mapper.mapToDto(expected)).thenReturn(expectedDto);
+        GiftCertificateDto actual = giftCertificateService.find(id);
+        assertEquals(expectedDto, actual);
+        verify(giftCertificateDao, times(1)).findOne(id);
+    }
+
+    @Test
+    public void findNegativeTest() {
+        Long id = 1L;
+        when(giftCertificateDao.findOne(id)).thenReturn(Optional.empty());
+        assertThrows(AppException.class, () -> {
+            giftCertificateService.find(id);
+        });
+        verify(giftCertificateDao, times(1)).findOne(id);
+    }
+
+    @Test
+    public void findByNamePositiveTest() {
+        String name = "GiftCertificate";
+        GiftCertificateDto expectedDto = new GiftCertificateDto();
+        expectedDto.setName(name);
+        GiftCertificate expected = new GiftCertificate();
+        expected.setName(name);
+        when(giftCertificateDao.findByName(name)).thenReturn(Optional.of(expected));
+        when(mapper.mapToDto(expected)).thenReturn(expectedDto);
+        GiftCertificateDto actual = giftCertificateService.findByName(name);
+        assertEquals(expectedDto, actual);
+        verify(giftCertificateDao, times(1)).findByName(name);
+    }
+
+    @Test
+    public void findByNameNegativeTest() {
+        String name = "GiftCertificate";
+        when(giftCertificateDao.findByName(name)).thenReturn(Optional.empty());
+        assertThrows(AppException.class, () -> {
+            giftCertificateService.findByName(name);
+        });
+        verify(giftCertificateDao, times(1)).findByName(name);
+    }
+
+    @Test
+    void findAllTest() {
+        GiftCertificate expected1 = new GiftCertificate();
+        expected1.setName("Certificate1");
+        expected1.setId(1L);
+        GiftCertificate expected2 = new GiftCertificate();
+        expected2.setName("Certificate2");
+        expected2.setId(2L);
+        List<GiftCertificate> listExpected = new ArrayList<>();
+        listExpected.add(expected1);
+        listExpected.add(expected2);
+        GiftCertificateDto expectedDto1 = new GiftCertificateDto();
+        expectedDto1.setName("Certificate1");
+        expectedDto1.setId(1L);
+        GiftCertificateDto expectedDto2 = new GiftCertificateDto();
+        expectedDto2.setName("Certificate2");
+        expectedDto2.setId(2L);
+        List<GiftCertificateDto> listDtoExpected = new ArrayList<>();
+        listDtoExpected.add(expectedDto1);
+        listDtoExpected.add(expectedDto2);
+
+        when(giftCertificateDao.findAll()).thenReturn(listExpected);
+        when(giftCertificateDao.getTags(expected1.getId())).thenReturn(any(List.class));
+        when(giftCertificateDao.getTags(expected2.getId())).thenReturn(any(List.class));
+        when(mapper.mapToListDto(listExpected)).thenReturn(listDtoExpected);
+        List<GiftCertificateDto> actual = giftCertificateService.findAll();
+
+        assertEquals(listDtoExpected, actual);
+        verify(giftCertificateDao, times(1)).findAll();
+    }
+
+    @Test
+    public void findByQueryPositiveTest() {
+        Query query = new Query();
+        query.setContains("Query");
+        QueryDto queryDto = new QueryDto();
+        queryDto.setContains("Query");
+        GiftCertificate expected1 = new GiftCertificate();
+        expected1.setName("Certificate1");
+        expected1.setId(1L);
+        GiftCertificate expected2 = new GiftCertificate();
+        expected2.setName("Certificate2");
+        expected2.setId(2L);
+        List<GiftCertificate> listExpected = new ArrayList<>();
+        listExpected.add(expected1);
+        listExpected.add(expected2);
+        GiftCertificateDto expectedDto1 = new GiftCertificateDto();
+        expectedDto1.setName("Certificate1");
+        expectedDto1.setId(1L);
+        GiftCertificateDto expectedDto2 = new GiftCertificateDto();
+        expectedDto2.setName("Certificate2");
+        expectedDto2.setId(2L);
+        List<GiftCertificateDto> listDtoExpected = new ArrayList<>();
+        listDtoExpected.add(expectedDto1);
+        listDtoExpected.add(expectedDto2);
+
+        when(queryMapper.mapToEntity(queryDto)).thenReturn(query);
+        when(giftCertificateDao.findByQuery(query)).thenReturn(listExpected);
+        when(giftCertificateDao.getTags(expected1.getId())).thenReturn(any(List.class));
+        when(giftCertificateDao.getTags(expected2.getId())).thenReturn(any(List.class));
+        when(mapper.mapToListDto(listExpected)).thenReturn(listDtoExpected);
+        List<GiftCertificateDto> actual = giftCertificateService.findByQuery(queryDto);
+
+        assertEquals(listDtoExpected, actual);
+        verify(giftCertificateDao, times(1)).findByQuery(query);
+    }
+
+//    @Test
+//    void updatePositiveTest() {
+//        Long id = 1L;
+//        GiftCertificateDto expectedDto = new GiftCertificateDto();
+//        expectedDto.setName("GiftCertificate");
+//        GiftCertificate expected = new GiftCertificate();
+//        expected.setName("GiftCertificate");
+//
+//        when(validator.isValidName(expectedDto.getName())).thenReturn(true);
+//        when(validator.isValidDescription(expectedDto.getDescription())).thenReturn(true);
+//        when(validator.isValidPrice(expectedDto.getPrice())).thenReturn(true);
+//        when(validator.isValidDuration(expectedDto.getDuration())).thenReturn(true);
+//        when(mapper.mapDtoToEntity(expectedDto)).thenReturn(expected);
+//        when(giftCertificateDao.update(expected, id)).thenReturn(expected);
+//        when(giftCertificateService.exist(expectedDto, id)).thenReturn(true);
+//        when(mapper.mapEntityToDto(expected)).thenReturn(expectedDto);
+//        GiftCertificateDto actual = giftCertificateService.update(expectedDto, id);
+//        assertEquals(expectedDto, actual);
+//        verify(giftCertificateDao, times(1)).update(any(GiftCertificate.class), any(Long.class));
+//    }
+//
+//    @Test
+//    void updateNegativeTest() {
+//        GiftCertificateDto expectedDto = new GiftCertificateDto();
+//        expectedDto.setName("GiftCertificate");
+//        GiftCertificate expected = new GiftCertificate();
+//        expected.setName("GiftCertificate");
+//
+//        when(validator.isValidName(expectedDto.getName())).thenReturn(false);
+//        assertThrows(CustomException.class,() -> {
+//            giftCertificateService.add(expectedDto);
+//        } );
+//        verify(giftCertificateDao,  never()).add(any(GiftCertificate.class));
+//    }
+
+}
